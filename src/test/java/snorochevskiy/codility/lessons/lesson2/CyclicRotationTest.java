f14485c69c473eda0b4dbe8ee18e1abf1bc9495b
Stas Norochevskiy <stas.norochevskiy@gmail.com>
2016-03-16 00:38:47 +0300

    No message

--- src/test/java/snorochevskiy/codility/lessons/lesson2/CyclicRotationTest.java
+++ src/test/java/snorochevskiy/codility/lessons/lesson2/CyclicRotationTest.java
@@ -1,7 +1,44 @@
 package snorochevskiy.codility.lessons.lesson2;
 
-/**
- * Created by stas on 3/16/16.
- */
+import org.junit.Assert;
+import org.junit.Test;
+
 public class CyclicRotationTest {
+
+    CyclicRotation sut = new CyclicRotation();
+
+    @Test
+    public void simpleTest() {
+        int[] src = {3, 8, 9, 7, 6};
+        int[] expected = {9, 7, 6, 3, 8};
+        Assert.assertArrayEquals(expected, sut.solution(src, 3));
+    }
+
+    @Test
+    public void testWithTooBigShiftFactor() {
+        int[] src = {3, 8, 9, 7, 6};
+        int[] expected = {9, 7, 6, 3, 8};
+        Assert.assertArrayEquals(expected, sut.solution(src, 8));
+    }
+
+    @Test
+    public void testOneElementShift() {
+        int[] src = {1, 2, 3, 4, 5};
+        int[] expected = {5, 1, 2, 3, 4};
+        Assert.assertArrayEquals(expected, sut.solution(src, 1));
+    }
+
+    @Test
+    public void testOneElementArray() {
+        int[] src = {1};
+        int[] expected = {1};
+        Assert.assertArrayEquals(expected, sut.solution(src, 1));
+    }
+
+    @Test
+    public void testEmptyArray() {
+        int[] src = {};
+        int[] expected = {};
+        Assert.assertArrayEquals(expected, sut.solution(src, 5));
+    }
 }
